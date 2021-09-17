185753d64be6021fd24b2361f48d861a7ba55c7d
Jeremy A Gray <jeremy.a.gray@gmail.com>
2021-09-17 13:03:22 -0500

    feat: add basic county routes
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

---
+++ routes/county.js
@@ -0,0 +1,32 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright 2020-2021 Jeremy A Gray <jeremy.a.gray@gmail.com>.
+ */
+
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const validation = require('../middleware/validation.js');
+
+const countyController = require('../controllers/county.js');
+
+router.get('/all',
+  countyController.getAll
+);
+
+router.get('/fips/:num',
+  validation.validateFips,
+  validation.validationErrorReporterJSON,
+  countyController.getCountyByFips
+);
+
+router.get('/name/:name',
+  validation.validateName,
+  validation.validationErrorReporterJSON,
+  countyController.getCountyByName
+);
+
+module.exports = router;
