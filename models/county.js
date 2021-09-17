6aea43b203308d3303cda893f05edf568af7b1e1
Jeremy A Gray <jeremy.a.gray@gmail.com>
2021-09-17 13:03:01 -0500

    feat: add basic county model
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

---
+++ models/county.js
@@ -0,0 +1,32 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright 2020-2021 Jeremy A Gray <jeremy.a.gray@gmail.com>.
+ */
+
+'use strict';
+
+const mongoose = require('mongoose');
+
+// Create a URL schema and model.
+const countySchema = new mongoose.Schema(
+  {
+    'name': {
+      'type': String,
+      'required': true
+    },
+    'fips': {
+      'type': String,
+      'required': true
+    },
+    'seat': {
+      'type': String,
+      'required': false
+    }
+  });
+
+function countyModel() {
+  return mongoose.model('County', countySchema);
+}
+
+module.exports = countyModel;
