83d846e9ef648dfa7d0f921443291ac7d48c8afb
Jeremy A Gray <jeremy.a.gray@gmail.com>
2021-09-22 21:39:19 -0500

    test(api): add initial GET all counties tests
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

---
+++ tests/county.js
@@ -0,0 +1,68 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright 2021 Jeremy A Gray <gray@flyquackswim.com>.
+ */
+
+'use strict';
+
+const chai = require('chai');
+const expect = chai.expect;
+const chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+
+const server = require('../server.js');
+// const County = require('../models/county.js');
+// const countyController = require('../controllers/county.js');
+
+describe('GET /api/v1/counties/all', async function() {
+
+  before('add the county data', async function() {
+    // const countyModel = County();
+    // await countyModel.deleteMany({});
+
+    // for (let i = 0; i < goodURLs.length; i++) {
+    //   // eslint-disable-next-line security/detect-object-injection
+    //   await createURL({'url': goodURLs[i].url});
+    // }
+
+    return;
+  });
+
+  after('clear the test database', async function() {
+    // await County().deleteMany({});
+
+    return;
+  });
+
+  it('should list all URLs', async function() {
+    try {
+      const response = await chai.request(server)
+        .get('/api/v1/counties/all');
+
+      expect(response).to.have.status(200);
+      expect(response.body.length).to.be.equal(67);
+      expect(response.body).to.be.a('array');
+
+      for (let i = 0; i < response.body.length; i++) {
+        // eslint-disable-next-line security/detect-object-injection
+        expect(response.body[i]).to.have.property('name');
+        // eslint-disable-next-line security/detect-object-injection
+        expect(response.body[i]).to.have.property('cname');
+        // eslint-disable-next-line security/detect-object-injection
+        expect(response.body[i]).to.have.property('fips');
+        // eslint-disable-next-line security/detect-object-injection
+        expect(response.body[i]).to.have.property('seat');
+        // eslint-disable-next-line security/detect-object-injection
+        expect(response.body[i]).to.have.property('established');
+        // eslint-disable-next-line security/detect-object-injection
+        expect(response.body[i]).to.have.property('code');
+      }
+
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  });
+});
