8f849846468f5122d920b5cc98411f98b587e31c
Jeremy A Gray <jeremy.a.gray@gmail.com>
2021-10-04 12:41:11 -0500

    feat(api): add county boundary model
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

---
+++ models/countyGeo.js
@@ -0,0 +1,30 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright 2020-2021 Jeremy A Gray <jeremy.a.gray@gmail.com>.
+ */
+
+'use strict';
+
+require('mongoose-geojson-schema');
+const mongoose = require('mongoose');
+
+// Create a county schema and model.
+const countySchema = new mongoose.Schema(
+  {
+    'name': {
+      'type': String,
+      'required': true
+    },
+    'fips': {
+      'type': String,
+      'required': true
+    },
+    'feature': mongoose.Schema.Types.Feature
+  });
+
+function countyModel() {
+  return mongoose.model('County', countySchema);
+}
+
+module.exports = countyModel;
