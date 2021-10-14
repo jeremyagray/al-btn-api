f411666af7197fde9d09084a2cfb4b65f89cd8e0
Jeremy A Gray <jeremy.a.gray@gmail.com>
2021-10-14 05:56:20 -0500

    fix(data): store centroids as geoJSON
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

--- bin/state-centroids.py
+++ bin/state-centroids.py
@@ -29,7 +29,18 @@ def get_state_centroids():
     url = "https://en.wikipedia.org/wiki/List_of_geographic_centers_of_the_United_States"
     raw = requests.get(url)
     soup = bs(raw.content, "html.parser")
-    centroids = []
+    centroids = {
+        "type": "FeatureCollection",
+        "name": "US States and Territories Centroids",
+        "crs": {
+            "type": "name",
+            "properties": {
+                "name": "urn:ogc:def:crs:EPSG::4269"
+            }
+        },
+        "features": [
+        ]
+    }
 
     tables = soup.find_all("table", class_="wikitable sortable")
     for table in tables:
@@ -39,13 +50,22 @@ def get_state_centroids():
             for row in table.tbody.find_all("tr"):
                 cells = row.find_all("td")
                 if len(cells):
-                    centroids.append({
-                        "state": cells[0].text.strip(),
-                        "latitude": float(cells[2].find("span", class_="geo").text.strip().split(";")[0].strip()),
-                        "longitude": float(cells[2].find("span", class_="geo").text.strip().split(";")[1].strip()),
+                    centroids["features"].append({
+                        "type": "Feature",
+                        "properties": {
+                            "state": cells[0].text.strip(),
+                        },
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": [
+                                float(cells[2].find("span", class_="geo").text.strip().split(";")[1].strip()),
+                                float(cells[2].find("span", class_="geo").text.strip().split(";")[0].strip()),
+                            ]
+                        }
                     })
 
-        # territories table caption:  Geographic centers of the U.S. territories
+        # territories table caption: Geographic centers of the
+        # U.S. territories
         if "Geographic centers of the U.S. territories" in table.caption.text.strip():
             for row in table.tbody.find_all("tr"):
                 cells = row.find_all("td")
@@ -53,14 +73,23 @@ def get_state_centroids():
                     if cells[3].find("span", class_="geo"):
                         coord = cells[3].find("span", class_="geo").text.strip()
 
-                        centroids.append({
-                            "state": cells[0].text.strip(),
-                            "latitude": float(coord.split(";")[0].strip()),
-                            "longitude": float(coord.split(";")[1].strip()),
+                        centroids["features"].append({
+                            "type": "Feature",
+                            "properties": {
+                                "state": cells[0].text.strip(),
+                            },
+                            "geometry": {
+                                "type": "Point",
+                                "coordinates": [
+                                    float(coord.split(";")[1].strip()),
+                                    float(coord.split(";")[0].strip()),
+                                ]
+                            }
                         })
 
     return centroids
 
 
 if __name__ == "__main__":
+    # update_state_centroids(get_state_centroids())
     print(json.dumps(get_state_centroids(), indent=2))
