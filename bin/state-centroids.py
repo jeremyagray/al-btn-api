886b9bd4a8b05e5afdacf3c4f71c370c32e70e0a
Jeremy A Gray <jeremy.a.gray@gmail.com>
2021-10-14 20:12:21 -0500

    feat(data): add argparse and finish updater
    Add `argparse` and some basic options and finish update logic.
    
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

--- bin/state-centroids.py
+++ bin/state-centroids.py
@@ -12,49 +12,42 @@
 
 import argparse
 import json
+
 import requests
-import sys
 from bs4 import BeautifulSoup as bs
 from pymongo import MongoClient
 
 
-def update_state_centroids(data):
+def update_state_centroids(
+    data,
+    conn="mongodb://localhost:27017/",
+    dbname="albtn",
+):
     """Load the state centroids."""
-    c = MongoClient("mongodb://localhost:27017/")
-    db = c["albtn"]
+    c = MongoClient(conn)
+    db = c[dbname]
     coll = db["stateboundaries"]
 
     for doc in data["features"]:
         coll.update_one(
-            {
-                "name": doc["properties"]["state"]
-            },
-            {
-                "$set": {
-                    "centroid": doc["geometry"]
-                }
-            },
+            {"name": doc["properties"]["state"]},
+            {"$set": {"centroid": doc["geometry"]}},
         )
 
     return
 
 
-def get_state_centroids():
+def get_state_centroids(
+    url="https://en.wikipedia.org/wiki/List_of_geographic_centers_of_the_United_States",
+):
     """Get and process the state and territory centroids."""
-    url = "https://en.wikipedia.org/wiki/List_of_geographic_centers_of_the_United_States"
     raw = requests.get(url)
     soup = bs(raw.content, "html.parser")
     centroids = {
         "type": "FeatureCollection",
         "name": "US States and Territories Centroids",
-        "crs": {
-            "type": "name",
-            "properties": {
-                "name": "urn:ogc:def:crs:EPSG::4269"
-            }
-        },
-        "features": [
-        ]
+        "crs": {"type": "name", "properties": {"name": "urn:ogc:def:crs:EPSG::4269"}},
+        "features": [],
     }
 
     tables = soup.find_all("table", class_="wikitable sortable")
@@ -65,19 +58,33 @@ def get_state_centroids():
             for row in table.tbody.find_all("tr"):
                 cells = row.find_all("td")
                 if len(cells):
-                    centroids["features"].append({
-                        "type": "Feature",
-                        "properties": {
-                            "state": cells[0].text.strip(),
-                        },
-                        "geometry": {
-                            "type": "Point",
-                            "coordinates": [
-                                float(cells[2].find("span", class_="geo").text.strip().split(";")[1].strip()),
-                                float(cells[2].find("span", class_="geo").text.strip().split(";")[0].strip()),
-                            ]
+                    centroids["features"].append(
+                        {
+                            "type": "Feature",
+                            "properties": {
+                                "state": cells[0].text.strip(),
+                            },
+                            "geometry": {
+                                "type": "Point",
+                                "coordinates": [
+                                    float(
+                                        cells[2]
+                                        .find("span", class_="geo")
+                                        .text.strip()
+                                        .split(";")[1]
+                                        .strip()
+                                    ),
+                                    float(
+                                        cells[2]
+                                        .find("span", class_="geo")
+                                        .text.strip()
+                                        .split(";")[0]
+                                        .strip()
+                                    ),
+                                ],
+                            },
                         }
-                    })
+                    )
 
         # territories table caption: Geographic centers of the
         # U.S. territories
@@ -88,25 +95,109 @@ def get_state_centroids():
                     if cells[3].find("span", class_="geo"):
                         coord = cells[3].find("span", class_="geo").text.strip()
 
-                        centroids["features"].append({
-                            "type": "Feature",
-                            "properties": {
-                                "state": cells[0].text.strip(),
-                                "geoid": "",
-                                "usps": "",
-                            },
-                            "geometry": {
-                                "type": "Point",
-                                "coordinates": [
-                                    float(coord.split(";")[1].strip()),
-                                    float(coord.split(";")[0].strip()),
-                                ]
+                        centroids["features"].append(
+                            {
+                                "type": "Feature",
+                                "properties": {
+                                    "state": cells[0].text.strip(),
+                                    "geoid": "",
+                                    "usps": "",
+                                },
+                                "geometry": {
+                                    "type": "Point",
+                                    "coordinates": [
+                                        float(coord.split(";")[1].strip()),
+                                        float(coord.split(";")[0].strip()),
+                                    ],
+                                },
                             }
-                        })
+                        )
 
     return centroids
 
 
+def load_data(args):
+    """Load centroid data."""
+    if args.file:
+        with open(args.file, "r") as f:
+            return json.load(f)
+    else:
+        return get_state_centroids(args.url)
+
+
+def _create_argument_parser():
+    """Create an argparse argument parser."""
+    parser = argparse.ArgumentParser(
+        description="""\
+al-btn-api, Alabama data API
+""",
+    )
+
+    parser.add_argument(
+        "-f",
+        "--file",
+        dest="file",
+        type=str,
+        default="",
+        # nargs=1,
+        help="Local data file.",
+    )
+
+    parser.add_argument(
+        "-p",
+        "--print",
+        dest="print",
+        default=False,
+        action="store_true",
+        help="Print data to be uploaded, as valid JSON, without uploading the data.",
+    )
+
+    # MongoDB name.
+    parser.add_argument(
+        "-d",
+        "--database",
+        dest="database",
+        type=str,
+        default="albtn",
+        nargs=1,
+        help="MongoDB database name.",
+    )
+
+    # MongoDB connection string.
+    parser.add_argument(
+        "-c",
+        "--connection",
+        dest="connection",
+        type=str,
+        default="mongodb://localhost:27017/",
+        nargs=1,
+        help="MongoDB connection string.",
+    )
+
+    # url to slurp
+    parser.add_argument(
+        "-u",
+        "--url",
+        dest="url",
+        type=str,
+        default="https://en.wikipedia.org/wiki/List_of_geographic_centers_of_the_United_States",
+        nargs=1,
+        help="Source URL containing centroid estimate data.",
+    )
+
+    return parser
+
+
 if __name__ == "__main__":
-    # update_state_centroids(get_state_centroids())
-    print(json.dumps(get_state_centroids(), indent=2))
+
+    # Get and parse cli arguments.
+    args = _create_argument_parser().parse_args()
+
+    # Fetch and load data.
+    data = load_data(args)
+
+    # Print or update data.
+    if args.print:
+        print(json.dumps(data, indent=2))
+    else:
+        update_state_centroids(data, args.connection, args.database)
