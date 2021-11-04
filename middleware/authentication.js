16ec384bae0bf9813843b4356420f2d8740cf77e
Jeremy A Gray <jeremy.a.gray@gmail.com>
2021-11-04 13:04:16 -0500

    feat(auth): add password login
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

---
+++ middleware/authentication.js
@@ -0,0 +1,63 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright 2021 Jeremy A Gray <gray@flyquackswim.com>.
+ */
+
+'use strict';
+
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const Strategy = require('passport-local').Strategy;
+const JwtStrategy = require('passport-jwt').Strategy;
+const tokenExtractor = require('passport-jwt').ExtractJwt.fromBodyField('token');
+
+// Middleware.
+const logger = require('../middleware/logger.js');
+
+// Models.
+const User = require('../models/user.js');
+
+const serializeUser = (user, cb) => {
+  cb(null, user._id);
+};
+
+exports.serializeUser = serializeUser;
+
+const deserializeUser = async (id, cb) => {
+  const userModel = new User();
+  try {
+    const user = await userModel.findOneById(id).exec();
+    cb(null, { '_id': user._id });
+  } catch (error) {
+    cb(error, null);
+  }
+};
+
+exports.deserializeUser = deserializeUser;
+
+const authenticatePassword = async (email, password, cb) => {
+  const userModel = new User();
+
+  try {
+    const user = await userModel.findOne({'email': email}).exec();
+
+    if (user && user.isValidPassword(password)) {
+      return cb(null, user);
+    }
+
+    return cb(null, false, { 'message': 'authentication failed' });
+  } catch (error) {
+    cb(error, null);
+  }
+};
+
+const PasswordStrategy = new Strategy(
+  {
+    'usernameField': 'email',
+    'passwordField': 'password'
+  },
+  authenticatePassword
+);
+
+exports.PasswordStrategy = PasswordStrategy;
---
+++ routes/authenticate.js
@@ -0,0 +1,37 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright 2020-2021 Jeremy A Gray <gray@flyquackswim.com>.
+ */
+
+'use strict';
+
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+
+// Middleware.
+const validation = require('../middleware/validation.js');
+const PasswordStrategy = require('../middleware/authentication.js').PasswordStrategy;
+const serializeUser = require('../middleware/authentication.js').serializeUser;
+const deserializeUser = require('../middleware/authentication.js').deserializeUser;
+
+// Controllers.
+const authController = require('../controllers/authenticate.js');
+
+passport.use(PasswordStrategy);
+passport.serializeUser(serializeUser);
+passport.deserializeUser(deserializeUser);
+
+router.post('/login/password',
+  validation.validateEmail,
+  validation.validatePassword,
+  validation.validationErrorReporterJSON,
+  passport.authenticate('local'),
+  function(request, response) {
+    console.log({ 'message': 'successful login' });
+    return response.json({ 'message': 'successful login' });
+  }
+);
+
+module.exports = router;
